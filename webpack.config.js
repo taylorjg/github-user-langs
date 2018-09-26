e8c49ab729f4d88cba0758464207f85f2f1b76ba
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-09-26 19:06:16 +0100

    Move source files to top level folders again (prep for using Angular)

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const packageJson = require('./package.json')
 
-const serverPublic = path.join(__dirname, 'src', 'server', 'public')
+const serverPublic = path.join(__dirname, 'server', 'public')
 
 module.exports = {
   entry: [
     'babel-polyfill',
-    './src/client/index.js'
+    './client/index.js'
   ],
   output: {
     path: serverPublic,
@@ -18,10 +18,10 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin([
-      { context: './src/client', from: '*.{html,css,gif}' }
+      { context: './client', from: '*.{html,css,gif}' }
     ]),
     new HtmlWebpackPlugin({
-      template: './src/client/index.html',
+      template: './client/index.html',
       version: packageJson.version
     })
   ],
