273b84dfebf3c26a536079ab963b53ed4c17e68c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-09-27 23:30:43 +0100

    Create basic GH service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from './services/github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    GithubService
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'github-user-langs';
+
+  constructor(private gh: GithubService) { }
+
+  ngOnInit() {
+    this.gh.getUserLangs('fluffy322').subscribe(
+      data => {
+        console.log(JSON.stringify(data, null, 2))
+      },
+      error => {
+        console.log(`ERROR: ${JSON.stringify(error, null, 2)}`)
+      })
+  }
 }
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  constructor(private http: HttpClient) { }
+
+  getUserLangs(username: String) {
+    return this.http.get(`/api/userLangs/${username}`);
+  }
+}
