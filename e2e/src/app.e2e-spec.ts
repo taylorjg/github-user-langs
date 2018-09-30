a3862c1421cb51e4d00aa8113dff83c2dcfc654b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-09-30 20:15:46 +0100

    Add first two e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,24 @@
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
+  
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should list 3 languages for dhpiggott', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to github-user-langs!');
+    page.enterUsername('dhpiggott');
+    page.clickSubmit();
+    expect(page.getResultsTableRows().count()).toBe(3);
+  });
+
+  it('should display an error message for non-existent user, "dhpiggott-bogus"', () => {
+    page.navigateTo();
+    page.enterUsername('dhpiggott-bogus');
+    page.clickSubmit();
+    expect(page.getErrorPanelErrorMessage()).toContain('dhpiggott-bogus');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,24 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+  
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  enterUsername(username) {
+    return element(by.css('#username')).sendKeys(username);
+  }
+
+  clickSubmit() {
+    return element(by.css('#submit')).click();
+  }
+
+  getResultsTableRows() {
+    return element.all(by.css('table tbody tr'));
+  }
+
+  getErrorPanelErrorMessage() {
+    return element(by.css('.alert #errorMessage')).getText();
   }
 }
