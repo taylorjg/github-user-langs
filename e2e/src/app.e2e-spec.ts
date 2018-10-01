039fda377d7498f01e4e36c5360196a8c48a9a4f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-10-01 21:55:32 +0100

    Add e2e tests re repo filter checkboxes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -32,4 +32,34 @@ describe('workspace-project App', () => {
     page.clickSubmit();
     expect(page.getErrorPanelErrorMessage()).toContain('dhpiggott-bogus');
   });
+
+  it('should have the correct repo count', async () => {
+    page.navigateTo('taylorjg');
+    expect(page.getRepoCount()).toBeGreaterThan(100);
+  });
+
+  it('should have higher repo count after checking includeForkedRepos', async () => {
+    page.navigateTo('taylorjg');
+    const baseCount = await page.getRepoCount();
+    page.setIncludeForkedReposValue(true);
+    expect(page.getRepoCount()).toBeGreaterThan(baseCount);
+  });
+
+  it('should have higher repo count after checking includeNonOwnedRepos', async () => {
+    page.navigateTo('taylorjg');
+    const baseCount = await page.getRepoCount();
+    page.setIncludeNonOwnedReposValue(true);
+    expect(page.getRepoCount()).toBeGreaterThan(baseCount);
+  });
+
+  it('should have even higher repo count after checking both checkboxes', async () => {
+    page.navigateTo('taylorjg');
+    const count1 = await page.getRepoCount();
+    page.setIncludeForkedReposValue(true);
+    const count2 = await page.getRepoCount();
+    page.setIncludeNonOwnedReposValue(true);
+    const count3 = await page.getRepoCount();
+    expect(count2).toBeGreaterThan(count1);
+    expect(count3).toBeGreaterThan(count2);
+  });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,7 +1,7 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  
+
   navigateTo(username?: string) {
     const destination = username ? `/index.html?username=${username}` : '/'
     return browser.get(destination);
@@ -25,5 +25,35 @@ export class AppPage {
 
   getErrorPanelErrorMessage() {
     return element(by.css('.alert #errorMessage')).getText();
+  }
+
+  getRepoCount() {
+    return element(by.css('#repoCount')).getText().then(Number);
+  }
+
+  getIncludeForkedReposValue() {
+    return element(by.css('#includeForkedRepos'))
+      .getAttribute('checked')
+      .then(value => !!value);
+  }
+
+  setIncludeForkedReposValue(value) {
+    const currentValue = this.getIncludeForkedReposValue();
+    if (currentValue !== value) {
+      return element(by.css('#includeForkedRepos')).click();
+    }
+  }
+
+  getIncludeNonOwnedReposValue() {
+    return element(by.css('#includeNonOwnedRepos'))
+      .getAttribute('checked')
+      .then(value => !!value);
+  }
+
+  setIncludeNonOwnedReposValue(value) {
+    const currentValue = this.getIncludeNonOwnedReposValue();
+    if (currentValue !== value) {
+      return element(by.css('#includeNonOwnedRepos')).click();
+    }
   }
 }
