142e8a4852519dcc18e9d95a66f4ad5346079061
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-10-03 19:51:20 +0100

    Split up e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,90 +2,103 @@ import { AppPage } from './app.po';
 import { version } from '../../version2';
 
 describe('github-user-langs', () => {
-  
+
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display the correct version number', () => {
-    page.navigateTo();
-    expect(page.getVersion()).toMatch(new RegExp(`${version}$`));
+  describe('general', () => {
+
+    it('should display the correct version number', () => {
+      page.navigateTo();
+      expect(page.getVersion()).toMatch(new RegExp(`${version}$`));
+    });
+
+    it('should display an error message for a non-existent user', () => {
+      page.navigateTo();
+      page.enterUsername('bogus-crap-blah');
+      page.clickSubmit();
+      expect(page.getErrorPanelErrorMessage()).toContain('bogus-crap-blah');
+    });
   });
 
-  it('should have non-zero language count for dhpiggott (via form submission)', () => {
-    page.navigateTo();
-    page.enterUsername('dhpiggott');
-    page.clickSubmit();
-    expect(page.getResultsTableRows().count()).toBeGreaterThan(0);
+  describe('dhpiggott', () => {
+
+    const USERNAME = 'dhpiggott';
+
+    it('should have non-zero language count (via form submission)', () => {
+      page.navigateTo();
+      page.enterUsername(USERNAME);
+      page.clickSubmit();
+      expect(page.getResultsTableRows().count()).toBeGreaterThan(0);
+    });
+
+    it('should have non-zero language count (via query param)', () => {
+      page.navigateTo(USERNAME);
+      expect(page.getResultsTableRows().count()).toBeGreaterThan(0);
+    });
+
+    it('should have languages that total 100% \u00b1 tolerance', async () => {
+      page.navigateTo(USERNAME);
+      const rows = page.getResultsTableRows();
+      const f = async (acc, row) => {
+        const s = await row.$('td:nth-child(2)').getText()
+        const n = Number(s.slice(0, -1))
+        return acc + n
+      }
+      const total = await rows.reduce(f, 0);
+      expect(total).toBeCloseTo(100, 6);
+    });
+
+    it('should fully populate the user details panel', () => {
+      page.navigateTo(USERNAME);
+      expect(page.getUserLink()).toBe('https://github.com/dhpiggott');
+      expect(page.getUserAvatarLink()).toBe('https://avatars1.githubusercontent.com/u/6965859?v=4');
+      expect(page.getUserNameText()).toBe('David Piggott');
+      expect(page.getUserLoginText()).toBe('dhpiggott');
+      expect(page.getUserCompanyText()).toBe('@cakesolutions');
+      expect(page.getUserLocationText()).toBe('United Kingdom');
+      expect(page.getUserEmailText()).toBe('david@piggott.me.uk');
+      expect(page.getUserEmailLink()).toBe('mailto:david@piggott.me.uk');
+      expect(page.getUserWebsiteText()).toBe('https://www.dhpiggott.net/');
+      expect(page.getUserWebsiteLink()).toBe('https://www.dhpiggott.net/');
+    });
   });
 
-  it('should have non-zero language count for dhpiggott (via query param)', () => {
-    page.navigateTo('dhpiggott');
-    expect(page.getResultsTableRows().count()).toBeGreaterThan(0);
-  });
+  describe('taylorjg', () => {
 
-  it('should display an error message for a non-existent user', () => {
-    page.navigateTo();
-    page.enterUsername('mary-mungo-midge');
-    page.clickSubmit();
-    expect(page.getErrorPanelErrorMessage()).toContain('mary-mungo-midge');
-  });
+    const USERNAME = 'taylorjg';
 
-  it('should total 100% \u00b1 tolerance', async () => {
-    page.navigateTo('dhpiggott');
-    const rows = page.getResultsTableRows();
-    const f = async (acc, row) => {
-      const s = await row.$('td:nth-child(2)').getText()
-      const n = Number(s.slice(0, -1))
-      return acc + n
-    }
-    const total = await rows.reduce(f, 0);
-    expect(total).toBeCloseTo(100, 6);
-  });
+    it('should have a non-zero repo count', async () => {
+      page.navigateTo(USERNAME);
+      expect(page.getRepoCount()).toBeGreaterThan(0);
+    });
 
-  it('should have a non-zero repo count', async () => {
-    page.navigateTo('dhpiggott');
-    expect(page.getRepoCount()).toBeGreaterThan(0);
-  });
+    it('should have higher repo count after checking includeForkedRepos', async () => {
+      page.navigateTo(USERNAME);
+      const baseCount = await page.getRepoCount();
+      page.setIncludeForkedReposValue(true);
+      expect(page.getRepoCount()).toBeGreaterThan(baseCount);
+    });
 
-  it('should have higher repo count after checking includeForkedRepos', async () => {
-    page.navigateTo('taylorjg');
-    const baseCount = await page.getRepoCount();
-    page.setIncludeForkedReposValue(true);
-    expect(page.getRepoCount()).toBeGreaterThan(baseCount);
-  });
+    it('should have higher repo count after checking includeNonOwnedRepos', async () => {
+      page.navigateTo(USERNAME);
+      const baseCount = await page.getRepoCount();
+      page.setIncludeNonOwnedReposValue(true);
+      expect(page.getRepoCount()).toBeGreaterThan(baseCount);
+    });
 
-  it('should have higher repo count after checking includeNonOwnedRepos', async () => {
-    page.navigateTo('taylorjg');
-    const baseCount = await page.getRepoCount();
-    page.setIncludeNonOwnedReposValue(true);
-    expect(page.getRepoCount()).toBeGreaterThan(baseCount);
-  });
-
-  it('should have even higher repo count after checking both checkboxes', async () => {
-    page.navigateTo('taylorjg');
-    const count1 = await page.getRepoCount();
-    page.setIncludeForkedReposValue(true);
-    const count2 = await page.getRepoCount();
-    page.setIncludeNonOwnedReposValue(true);
-    const count3 = await page.getRepoCount();
-    expect(count2).toBeGreaterThan(count1);
-    expect(count3).toBeGreaterThan(count2);
-  });
-
-  it('should populate the user details panel', () => {
-    page.navigateTo('DHPIGGOTT');
-    expect(page.getUserLink()).toBe('https://github.com/dhpiggott');
-    expect(page.getUserAvatarLink()).toBe('https://avatars1.githubusercontent.com/u/6965859?v=4');
-    expect(page.getUserNameText()).toBe('David Piggott');
-    expect(page.getUserLoginText()).toBe('dhpiggott');
-    expect(page.getUserCompanyText()).toBe('@cakesolutions');
-    expect(page.getUserLocationText()).toBe('United Kingdom');
-    expect(page.getUserEmailText()).toBe('david@piggott.me.uk');
-    expect(page.getUserEmailLink()).toBe('mailto:david@piggott.me.uk');
-    expect(page.getUserWebsiteText()).toBe('https://www.dhpiggott.net/');
-    expect(page.getUserWebsiteLink()).toBe('https://www.dhpiggott.net/');
+    it('should have even higher repo count after checking both checkboxes', async () => {
+      page.navigateTo(USERNAME);
+      const count1 = await page.getRepoCount();
+      page.setIncludeForkedReposValue(true);
+      const count2 = await page.getRepoCount();
+      page.setIncludeNonOwnedReposValue(true);
+      const count3 = await page.getRepoCount();
+      expect(count2).toBeGreaterThan(count1);
+      expect(count3).toBeGreaterThan(count2);
+    });
   });
 });
